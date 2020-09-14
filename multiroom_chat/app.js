0eca0baf216a02cbceb4c772b528ed6856741c73
KalebLight <kaleb.luiz00@gmail.com>
2020-09-14 05:00:25 -0300

    Implementando atualização da lista de participantes da conversa

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -19,12 +19,26 @@ io.on('connection', function(socket){
     socket.on('msgParaServidor', function(data){
         socket.emit(
             'msgParaCliente', 
-            { apelido: data.apelido, mensagem: data.mensagem})
+            { apelido: data.apelido, mensagem: data.mensagem}
+        );
         
         socket.broadcast.emit(
             'msgParaCliente', 
-            { apelido: data.apelido, mensagem: data.mensagem})
-
+            { apelido: data.apelido, mensagem: data.mensagem}
+        );
+   
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            );
+                
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            ); 
+        }
+        
     })
     
     
