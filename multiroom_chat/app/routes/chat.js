6f789265d48e9b08af6fde4f295ba46b20ddfab9
Fakaleb <57027004+Fakaleb@users.noreply.github.com>
2020-10-04 13:53:39 -0300

    Update chat.js

--- multiroom_chat/app/routes/chat.js
+++ multiroom_chat/app/routes/chat.js
@@ -2,6 +2,15 @@ module.exports = function(application){
 
     application.post('/chat', function(req, res){
         application.app.controllers.chat.iniciaChat(application, req, res)
+        
+    var connection = application.config.dbConnection()        
+    var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    var id_noticia = req.query
+
+    noticiasModel.getNoticia(id_noticia, function(error, result){
+        res.render("noticias/noticia", {noticia : result} )
+    }) 
     })
 
     application.get('/chat', function(req, res){
